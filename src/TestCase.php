6490c4fe2d72d969310780f465232b802450d32c
Matthew Baggett <mbaggett@tradetracker.com>
2019-06-17 15:06:08 +0200

    Implement request() method from upstream

--- src/TestCase.php
+++ src/TestCase.php
@@ -5,6 +5,15 @@ namespace Gone\Testing;
 use Faker\Factory as FakerFactory;
 use Faker\Generator;
 use Faker\Provider;
+use Gone\AppCore\App;
+use Psr\Http\Message\ResponseInterface;
+use Gone\AppCore\Router\Router;
+use Slim\Http\Environment;
+use Slim\Http\Headers;
+use Slim\Http\Request;
+use Slim\Http\RequestBody;
+use Slim\Http\Response;
+use Slim\Http\Uri;
 
 class TestCase extends \PHPUnit\Framework\TestCase
 {
@@ -14,6 +23,17 @@ class TestCase extends \PHPUnit\Framework\TestCase
 	 * @var Generator $faker
 	 */
 	static private $faker;
+
+	private $defaultEnvironment = [];
+	private $defaultHeaders     = [];
+
+	public function setUp(){
+		$this->defaultEnvironment = [
+			'SCRIPT_NAME' => '/index.php',
+			'RAND'        => rand(0, 100000000),
+		];
+		$this->defaultHeaders = [];
+	}
 
 	static public function setUpBeforeClass()
 	{
@@ -28,6 +48,104 @@ class TestCase extends \PHPUnit\Framework\TestCase
 		self::$faker->addProvider(new Provider\en_US\Address(self::$faker));
 		self::$faker->addProvider(new Provider\en_US\PhoneNumber(self::$faker));
 		self::$faker->addProvider(new Provider\en_US\Company(self::$faker));
+	}
+
+	private function getApp(){
+		return App::Instance();
+	}
+
+	/**
+	 * @param string $method
+	 * @param string $path
+	 * @param array  $post
+	 * @param bool   $isJsonRequest
+	 * @param array  $extraHeaders
+	 *
+	 * @return ResponseInterface
+	 */
+	public function request(
+		string $method,
+		string $path,
+		$post = null,
+		$isJsonRequest = true,
+		$extraHeaders = []
+	) {
+		/**
+		 * @var \Slim\App           $app
+		 * @var \Gone\AppCore\App $applicationInstance
+		 */
+		$applicationInstance = $this->getApp();
+		$calledClass = get_called_class();
+
+		$app = $applicationInstance->getApp();
+
+		if (defined("$calledClass")) {
+			$modelName = $calledClass::MODEL_NAME;
+			if (file_exists(APP_ROOT . "/src/Routes/{$modelName}Route.php")) {
+				require(APP_ROOT . "/src/Routes/{$modelName}Route.php");
+			}
+		} else {
+			if (file_exists(APP_ROOT . "/src/Routes.php")) {
+				require(APP_ROOT . "/src/Routes.php");
+			}
+		}
+		if (file_exists(APP_ROOT . "/src/RoutesExtra.php")) {
+			require(APP_ROOT . "/src/RoutesExtra.php");
+		}
+		Router::Instance()->populateRoutes($app);
+		$headers = array_merge($this->defaultHeaders, $extraHeaders);
+
+		$envArray = array_merge($this->defaultEnvironment, $headers);
+		$envArray = array_merge($envArray, [
+			'REQUEST_URI'    => $path,
+			'REQUEST_METHOD' => $method,
+		]);
+
+		$env     = Environment::mock($envArray);
+		$uri     = Uri::createFromEnvironment($env);
+		$headers = Headers::createFromEnvironment($env);
+
+		$cookies      = [];
+		$serverParams = $env->all();
+		$body         = new RequestBody();
+		if (!is_array($post) && $post != null) {
+			$body->write($post);
+			$body->rewind();
+		} elseif (is_array($post) && count($post) > 0) {
+			$body->write(json_encode($post));
+			$body->rewind();
+		}
+
+		$request = new Request($method, $uri, $headers, $cookies, $serverParams, $body);
+		if ($isJsonRequest) {
+			foreach ($extraHeaders as $k => $v) {
+				$request = $request->withHeader($k, $v);
+			}
+			$request = $request->withHeader("Content-type", "application/json");
+			$request = $request->withHeader("Accept", "application/json");
+		}
+		$response = new Response();
+
+		// Invoke app
+		$response = $applicationInstance
+			->makeClean()
+			->getApp()
+			->process($request, $response);
+		$response->getBody()->rewind();
+
+		return $response;
+	}
+
+	protected function setEnvironmentVariable($key, $value)
+	{
+		$this->defaultEnvironment[$key] = $value;
+		return $this;
+	}
+
+	protected function setRequestHeader($header, $value)
+	{
+		$this->defaultHeaders[$header] = $value;
+		return $this;
 	}
 
 
